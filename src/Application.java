df14226caeed869c2fe5fcd4e25937b404874fc4
My PC <hansathsara@gmial.com>
2026-08-14 16:29:44 +0530

    2

--- src/Application.java
+++ src/Application.java
@@ -2,37 +2,92 @@ import java.util.Scanner;
 
 public class Application {
 
+
     public static void printPrimaryQ() {
 
-        String[] questions = {
-                "=========================================================================",
-                "======================== Hello, Welcome to the CLI-POS ==========================",
 
-                "==================================",
-                "choose an option (Enter the number)",
-                "==================================",
+        System.out.println("==================================");
+        System.out.println("choose an option (Enter the number)");
+        System.out.println("==================================");
+        System.out.println("1) Customer Managment");
+        System.out.println("2) Product Managment");
+        System.out.println("3) Order Managment");
+        System.out.println("4) exit");
+        System.out.println("=========================================================================");
 
-                "1) Customer Managment",
-                "2) Product Managment",
-                "3) Order Managment",
-                "4) exit",
-                "=========================================================================",
 
-        };
+    }
 
-        for (String question : questions) {
+    public static void printWelcomeNote() {
+        System.out.println("=========================================================================");
+        System.out.println("======================== Hello, Welcome to the CLI-POS ==========================");
 
-            System.out.println(question);
+
+    }
+
+    static String[] customerQ = {
+            "1)Save Customer",
+            "2)Update Customer",
+            "3)Delete Customer",
+            "4)Search Customer",
+            "5)Back",
+            "6)Exit"
+    };
+
+
+    public static void manageCustomer() {
+        System.out.println("Manage your Customers");
+
+        for (String customerQ : customerQ) {
+
+            System.out.println(customerQ);
+
         }
 
 
     }
 
+    public static void manageProduct() {
+        System.out.println("Manage Your Products");
+    }
+
+    public static void manageOrder() {
+        System.out.println("Manage your Orders");
+    }
+
+
     public static void main(String[] args) {
 
-        printPrimaryQ();
-        Scanner input = new Scanner(System.in);
-        int num = input.nextInt();
+        // print welcome Note first
+        printWelcomeNote();
+
+
+        while (true) {
+            // print primary questions secondly
+            printPrimaryQ();
+
+            // user to allocate entet  opinion of managing
+            Scanner input = new Scanner(System.in);
+            int num = input.nextInt();
+
+            switch (num) {
+
+                case 1:
+                    manageCustomer();
+                    break;
+                case 2:
+                    manageProduct();
+                    break;
+                case 3:
+                    manageOrder();
+                    break;
+                default:
+                    System.out.println("Idiot..Try Again..");
+                    return;
+
+            }
+        }
+
 
     }
 }
